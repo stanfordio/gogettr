11d938f6fb68eda6a149068275ac6abaaea74e61
R. Miles McCain <github@sendmiles.email>
2021-07-02 18:50:18 +0000

    Add support for pulling all posts

---
+++ lib/capabilities/all_posts.py
@@ -0,0 +1,61 @@
+from typing import Iterator, Literal
+from lib.utils import b36decode, b36encode, merge
+from lib.capabilities.base import Capability
+
+
+class AllPosts(Capability):
+    def pull(
+        self,
+        first: str = None,
+        last: str = None,
+        max: int = None,
+        order: Literal["up", "down"] = "up",
+    ) -> Iterator[dict]:
+        # We remove the first character from the post IDs below because they are always `p` and not part of the numbering scheme
+        if order == "up":
+            post_id = b36decode(first[1:]) if first is not None else 1
+            end_at = b36decode(last[1:]) if last is not None else None
+        else:
+            if last is None:
+                raise ValueError(
+                    "the last post (i.e., the starting post) must be defined when pulling posts reverse chronologically (we need to know where to start!)"
+                )
+            post_id = b36decode(last[1:])
+            end_at = b36decode(first[1:]) if first is not None else 1
+
+        n = 0  # How many posts we've emitted
+
+        while (
+            end_at is None
+            or (order == "up" and post_id <= end_at)
+            or (order == "down" and post_id >= end_at)
+        ) and (max is None or n < max):
+            data = self.client.get(
+                f"/u/post/p{b36encode(post_id)}",
+                params={
+                    "incl": "poststats|userinfo",
+                },
+                key="result",
+            )
+
+            if order == "up":
+                post_id += 1
+            else:
+                post_id -= 1
+
+            if data["data"]["txt"] == "Content Not Found":
+                # Yes, this is how they do it. It's just a string.
+                continue
+
+            # At this point we know the post exists. Let's assemble and yield it.
+            user_id = data["data"]["uid"]
+            post = merge(
+                data["data"],
+                dict(
+                    uinf=data["aux"]["uinf"][user_id],
+                    shrdpst=data["aux"]["shrdpst"],
+                    s_pst=data["aux"]["s_pst"],
+                ),
+            )
+            n += 1
+            yield post
---
+++ tests/test_all_posts.py
@@ -0,0 +1,21 @@
+from lib.public import PublicClient
+
+client = PublicClient()
+
+
+def test_all_posts():
+    """Verifies we can extract the first five posts."""
+    posts = list(client.all_posts(max=5))
+    assert len(posts) == 5
+
+
+def test_user_posts_downward():
+    """Verifies that we can pull posts backward."""
+    posts = list(client.all_posts(last="pew9", max=10, order="down"))
+    assert len(posts) == 10
+
+
+def test_user_posts_limited_id():
+    """Verifies that we can pull posts backward with an ID as the endpoint."""
+    posts = list(client.all_posts(last="pew9", first="pew0", order="down"))
+    assert len(posts) > 0
