97948738adf9db1652aec796d3a50a07a79c6d10
R. Miles McCain <github@sendmiles.email>
2021-07-02 18:49:21 +0000

    Add utilities

---
+++ lib/utils.py
@@ -0,0 +1,23 @@
+def merge(*dicts):
+    """Merges the given dictionaries into a single dictionary, ignoring overlapping keys."""
+
+    d = dict()
+    for dictionary in dicts:
+        for (k, v) in dictionary.items():
+            d[k] = v
+    return d
+
+# Following two functions adapted from https://stackoverflow.com/questions/1181919/python-base-36-encoding
+def b36encode(number: int) -> str:
+    """Convert the number to base36."""
+    alphabet, base36 = ['0123456789abcdefghijklmnopqrstuvwxyz', '']
+
+    while number:
+        number, i = divmod(number, 36)
+        base36 = alphabet[i] + base36
+
+    return base36 or alphabet[0]
+
+def b36decode(number: int):
+    """Convert the base36 number to an integer."""
+    return int(number, 36)
