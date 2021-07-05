1d7052c58f09817537352aa0943b0b63d791c3e8
R. Miles McCain <github@sendmiles.email>
2021-07-05 18:33:23 +0000

    Add support for suggested users

---
+++ lib/capabilities/suggested.py
@@ -0,0 +1,30 @@
+from typing import Iterator, Literal
+from lib.capabilities.base import Capability
+
+
+class Suggested(Capability):
+    def pull(self, max: int = None) -> Iterator[dict]:
+        """Pull the suggested users.
+
+        :param int max: the maximum number of followers to pull"""
+
+        # https://api.gettr.com/s/usertag/suggest?offset=0&max=16&incl=userinfo%7Cfollowings
+        url = "/s/usertag/suggest"
+
+        n = 0  # Number of users emitted
+
+        for data in self.client.get_paginated(
+            url,
+            params={
+                "max": 16,
+                "incl": "userinfo|followings",
+            },
+            offset_step=16,
+        ):
+            for username in data["data"]["list"]:
+                # Verify that we haven't passed the max number of posts
+                if max is not None and n >= max:
+                    break
+
+                n += 1
+                yield data["aux"]["uinf"][username]
---
+++ tests/test_suggested.py
@@ -0,0 +1,11 @@
+from lib import PublicClient
+
+client = PublicClient()
+
+
+def test_suggested():
+    """Verifies we can extract suggested users."""
+    resp = list(client.suggested(max=50))
+    assert len(resp) == 50
+    assert "nickname" in resp[0]  # Verify that it is actual user data
+    assert "dsc" in resp[0]
