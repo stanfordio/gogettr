262bc3ce706835eaf6188d96d8b1a7ff1f992b43
KonradIT <github@chernowii.com>
2021-07-12 19:05:39 +0200

    Fix registration status test

--- tests/test_registration_status.py
+++ tests/test_registration_status.py
@@ -6,8 +6,8 @@ client = PublicClient()
 
 
 def test_registration_status_found():
-    assert client.is_registered("dineshdsouza")
+    assert client.is_registered("dineshdsouza").get("registered")
 
 
 def test_registration_status_not_found():
-    assert not client.is_registered("fekewgkfgewkhfgewkhfg")
+    assert not client.is_registered("fekewgkfgewkhfgewkhfg").get("registered")
