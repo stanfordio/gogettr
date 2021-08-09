a13b1bc58ce0366509cc40967fef741e1c6cb3a7
R. Miles McCain <oci@sendmiles.email>
2021-08-09 18:40:01 +0000

    Formatting

--- tests/test_hashtags.py
+++ tests/test_hashtags.py
@@ -8,7 +8,7 @@ client = PublicClient()
 def test_suggested_hashtags():
     """Verifies we can extract suggested hashtags."""
     resp = list(client.hashtags(max=10))
-    assert len(resp) == 5 # max seems to be 5 now
+    assert len(resp) == 5  # max seems to be 5 now
 
     # Later results (e.g., past #5) won't have expanded info
     assert "hashtag" in resp[0]
