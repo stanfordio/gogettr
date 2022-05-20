6bdfb6032597766307a372f39a41fc60647e1926
R. Miles McCain <github@sendmiles.email>
2022-05-20 14:31:52 -0700

    Fix search tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -7,11 +7,11 @@ client = PublicClient()
 
 def test_basic_search():
     """Verifies we can perform a basic search."""
-    posts = list(client.search(query="America", max=200))
+    posts = list(client.search(query="stanford", max=200))
     assert len(posts) > 50  # GETTR's number of search results is inconsistent
 
     for post in posts:
-        assert "america" in post["txt"].lower()
+        assert "stanford" in post["txt"].lower()
 
         # Check whether user info is present
         assert "ousername" in post["uinf"]
